62d014ad0f74153fff9f2615cf691573fae10ec1
LtARQ <qabdulraouf2@gmail.com>
2024-05-04 21:10:13 +0500

    logo update

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
         appearance={{
           layout: {
             socialButtonsVariant: "iconButton",
-            logoImageUrl: "/icons/yoom-logo.svg",
+            logoImageUrl: "/icons/logo.svg",
           },
           variables: {
             colorText: "#fff",
@@ -38,7 +38,7 @@ export default function RootLayout({
           },
         }}
       >
-        <body className={`{inter.className} bg-dark-2`}>
+        <body className={`${inter.className} bg-dark-2`}>
           {children}
           <Toaster />
         </body>
